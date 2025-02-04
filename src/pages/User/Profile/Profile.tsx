9a8259b048c229b498e648ba9325a87629d9752b
duckies <voducminh39@gmail.com>
2025-02-04 18:45:19 +0700

    feat: done

--- src/pages/User/Profile/Profile.tsx
+++ src/pages/User/Profile/Profile.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getUserProfile } from '../../../utils/auth'
+import { User } from '../../../types/user.type'
+import { useQuery } from '@tanstack/react-query'
 
 export default function Profile() {
+  const getProfileQuery = useQuery({
+    queryKey: ['profile'],
+    queryFn: () => getUserProfile()
+  })
+  const userProfile = getProfileQuery.data
+  console.log(userProfile)
   return (
     <div>
       <div className='p-3 border-b-2 border-gray-300'>
@@ -12,25 +21,37 @@ export default function Profile() {
             <tr className='p-2 h-10'>
               <td className='pr-4'>Email</td>
               <td>
-                <input className='h-10 w-full border border-gray-300 focus:outline-none' type='text' />
+                <input
+                  className='p-2 h-10 w-full border border-gray-300 focus:outline-none'
+                  type='text'
+                  value={userProfile?.email}
+                />
               </td>
             </tr>
             <tr className='p-2 h-10'>
               <td className='pr-4'>Id</td>
               <td>
-                <input className='h-10 w-full border border-gray-300 focus:outline-none' type='text' />
+                <input
+                  className='h-10 w-full border border-gray-300 focus:outline-none p-2'
+                  value={userProfile?._id}
+                  type='text'
+                />
               </td>
             </tr>
             <tr className='p-2 h-10'>
               <td className='pr-4'>Create at</td>
               <td>
-                <input className='h-10 w-full border border-gray-300 focus:outline-none' type='text' />
+                <input
+                  className='h-10 w-full border border-gray-300 focus:outline-none p-2'
+                  value={userProfile?.createdAt}
+                  type='text'
+                />
               </td>
             </tr>
             <tr className='p-2 h-10'>
               <td className='pr-4'>Role</td>
               <td>
-                <input className='h-10 w-full border border-gray-300 focus:outline-none' type='text' />
+                <input className='h-10 w-full border border-gray-300 focus:outline-none p-2' value={userProfile?.roles} type='text' />
               </td>
             </tr>
           </tbody>
--- src/pages/User/Purchase/Purchase.tsx
+++ src/pages/User/Purchase/Purchase.tsx
@@ -1,33 +1,53 @@
 import React, { useState } from 'react'
 import UserPurchase from '../../../components/User/UserPurchase'
+import { useQuery } from '@tanstack/react-query'
+import { purchaseStatus } from '../../../constants/purchases'
+import { createSearchParams, NavLink } from 'react-router-dom'
+import classNames from 'classnames'
+import { useQueryParams } from '../../../hooks/useQueryParams'
+import purchaseApi from '../../../api/purchase.api'
+import { PurchaseListStatus } from '../../../types/purchase.type'
 
 export default function Purchase() {
-  const [activeTab, setActiveTab] = useState('all')
-
+  // const [activeTab, setActiveTab] = useState('all')
+  const queryParams = useQueryParams()
+  const status = Number(queryParams.status)
+  console.log(status)
   const tabs = [
-    { id: 'all', label: 'Tất cả' },
-    { id: 'waiting_payment', label: 'Chờ thanh toán' },
-    { id: 'shipping', label: 'Vận chuyển' },
-    { id: 'waiting_delivery', label: 'Chờ giao hàng' },
-    { id: 'completed', label: 'Hoàn thành' },
-    { id: 'cancelled', label: 'Đã hủy' },
-    { id: 'return_refund', label: 'Trả hàng/Hoàn tiền' }
+    { id: purchaseStatus.all, name: 'Tất cả' },
+    { id: purchaseStatus.waitingForConfirmation, name: 'Chờ xác nhận' },
+    { id: purchaseStatus.waitingForGetting, name: 'Chờ lấy hàng' },
+    { id: purchaseStatus.inProgress, name: 'Đang giao' },
+    { id: purchaseStatus.delivered, name: 'Đã giao' },
+    { id: purchaseStatus.cancelled, name: 'Đã hủy' }
   ]
+
+  const getPurchasesQuery = useQuery({
+    queryKey: ['product', { status: status }],
+    queryFn: () => purchaseApi.getPurchases({ status: status as PurchaseListStatus })
+  })
+
+  const purchases = getPurchasesQuery.data?.data.data || []
 
   return (
     <div className='p-4'>
-      {/* Tabs */}
       <div className='flex border-b-2 border-gray-200 p-2 bg-white'>
         {tabs.map((tab) => (
-          <button
+          <NavLink
+            to={{
+              pathname: '/account/my-purchase',
+              search: createSearchParams({
+                status: String(tab.id)
+              }).toString()
+            }}
             key={tab.id}
-            onClick={() => setActiveTab(tab.id)}
-            className={`p-3 text-gray-600 ${
-              activeTab === tab.id ? 'text-red-700 border-b-2 font-bold border-red-700' : ''
-            }`}
+            className={classNames('p-3 text-gray-600', {
+              'text-red-700 border-b-2 font-bold border-red-700': tab.id === status,
+              '': tab.id !== status
+            })}
           >
-            {tab.label}
-          </button>
+            {tab.name}
+          </NavLink>
         ))}
       </div>
 
@@ -41,7 +61,9 @@ export default function Purchase() {
       </div>
 
       {/* Empty state */}
-      <UserPurchase />
+      {purchases.map((item) => (
+        <UserPurchase key={item._id} purchase ={item}/>
+      ))}
     </div>
   )
 }
